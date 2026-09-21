7a0f6a211fa84895adba0d65abd3077da4df00d0
abd ulwahab <absksbwj@gmail.com>
2026-09-21 15:49:55 +0300

    Update website code

--- script.js
+++ script.js
@@ -137,3 +137,22 @@ async function updateLanyard() {
 // تشغيل الدالة فوراً والتكرار كل 6 ثوانٍ
 updateLanyard();
 setInterval(updateLanyard, 6000);
+
+//
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    const targetId = this.getAttribute('href');
+    if (targetId === '#' || !targetId) return;
+
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      e.preventDefault();
+      
+      targetElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+  });
+});
